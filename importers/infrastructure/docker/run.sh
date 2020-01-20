3a4b058436d37bd3761c2e4ebb19954b7103335b
Jesper Kristensen <cableman@linuxdev.dk>
2020-01-20 14:37:21 +0100

    DDBTEAM-381: Updated docker to use base images

--- importers/infrastructure/docker/run.sh
+++ importers/infrastructure/docker/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 (cd cover-service-importers && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service-importers/cover-service-importers .)
+(cd cover-service-jobs && docker build --no-cache --tag=docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs .)
 
-#docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-importers:latest
+#docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service-importers/cover-service-importers:latest
+#docker push docker.pkg.github.com/danskernesdigitalebibliotek/ddb-cover-service/cover-service-jobs:latest
