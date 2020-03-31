0e5ede4908081e67b185baecfb527facea740033
Jesper Kristensen <cableman@linuxdev.dk>
2020-03-31 15:28:07 +0200

    DDBTEAM-442: Small debug fixes to upload queue processor

--- importers/src/Queue/CoverUploadProcessor.php
+++ importers/src/Queue/CoverUploadProcessor.php
@@ -10,6 +10,8 @@ namespace App\Queue;
 use App\Entity\Source;
 use App\Entity\Vendor;
 use App\Event\VendorEvent;
+use App\Repository\SourceRepository;
+use App\Repository\VendorRepository;
 use App\Utils\Message\CoverUploadProcessMessage;
 use App\Utils\Types\VendorState;
 use Doctrine\ORM\EntityManagerInterface;
@@ -31,6 +33,9 @@ class CoverUploadProcessor implements Processor, TopicSubscriberInterface
     private $statsLogger;
 
     private const VENDOR_ID = 12;
+    /** @var Vendor $vendor */
+    private $vendor;
+    private $sourceRepo;
 
     /**
      * CoverUploadProcessor constructor.
@@ -38,12 +43,19 @@ class CoverUploadProcessor implements Processor, TopicSubscriberInterface
      * @param EntityManagerInterface $entityManager
      * @param LoggerInterface $statsLogger
      * @param EventDispatcherInterface $eventDispatcher
+     * @param SourceRepository $sourceRepo
+     * @param VendorRepository $vendorRepo
      */
-    public function __construct(EntityManagerInterface $entityManager, LoggerInterface $statsLogger, EventDispatcherInterface $eventDispatcher)
+    public function __construct(EntityManagerInterface $entityManager, LoggerInterface $statsLogger, EventDispatcherInterface $eventDispatcher, SourceRepository $sourceRepo, VendorRepository $vendorRepo)
     {
         $this->em = $entityManager;
         $this->statsLogger = $statsLogger;
         $this->dispatcher = $eventDispatcher;
+
+        $this->sourceRepo = $sourceRepo;
+
+        // Load vendor here to ensure that it's only load once.
+        $this->vendor = $vendorRepo->find(self::VENDOR_ID);
     }
 
     /**
@@ -54,16 +66,10 @@ class CoverUploadProcessor implements Processor, TopicSubscriberInterface
         $jsonDecoder = new JsonDecoder(true);
         /** @var CoverUploadProcessMessage $uploadProcessMessage */
         $uploadProcessMessage = $jsonDecoder->decode($message->getBody(), CoverUploadProcessMessage::class);
-        $identifier = $uploadProcessMessage->getIdentifierType();
+        $identifier = $uploadProcessMessage->getIdentifier();
 
-        // @TODO: Could this be optimized to only be loaded once!
-        $vendorRepo = $this->em->getRepository(Vendor::class);
-        /** @var Vendor $vendor */
-        $vendor = $vendorRepo->find(self::VENDOR_ID);
-
-        $sourceRepo = $this->em->getRepository(Source::class);
         /** @var Source[] $sources */
-        $sources = $sourceRepo->findByMatchIdList($uploadProcessMessage->getIdentifierType(), [$identifier => ''], $vendor);
+        $sources = $this->sourceRepo->findByMatchIdList($uploadProcessMessage->getIdentifierType(), [$identifier => ''], $this->vendor);
 
         /**
          * @TODO: Add support for delete $uploadProcessMessage->getOperation() === VendorState::DELETE
@@ -73,7 +79,7 @@ class CoverUploadProcessor implements Processor, TopicSubscriberInterface
             $source = $sources[$identifier];
             $source->setMatchType($uploadProcessMessage->getIdentifierType())
                 ->setMatchId($identifier)
-                ->setVendor($vendor)
+                ->setVendor($this->vendor)
                 ->setDate(new \DateTime())
                 ->setOriginalFile($uploadProcessMessage->getImageUrl());
             $isNew = false;
@@ -81,7 +87,7 @@ class CoverUploadProcessor implements Processor, TopicSubscriberInterface
             $source = new Source();
             $source->setMatchType($uploadProcessMessage->getIdentifierType())
                 ->setMatchId($identifier)
-                ->setVendor($vendor)
+                ->setVendor($this->vendor)
                 ->setDate(new \DateTime())
                 ->setOriginalFile($uploadProcessMessage->getImageUrl());
             $this->em->persist($source);
@@ -89,14 +95,14 @@ class CoverUploadProcessor implements Processor, TopicSubscriberInterface
 
         // Make it stick and clean up memory.
         $this->em->flush();
-        $this->em->clear();
+        $this->em->clear(Source::class);
         gc_collect_cycles();
 
         if ($isNew) {
-            $event = new VendorEvent(VendorState::INSERT, [$identifier], $uploadProcessMessage->getIdentifierType(), $vendor);
+            $event = new VendorEvent(VendorState::INSERT, [$identifier], $uploadProcessMessage->getIdentifierType(), $this->vendor->getId());
             $this->dispatcher->dispatch($event::NAME, $event);
         } else {
-            $event = new VendorEvent(VendorState::UPDATE, [$identifier], $uploadProcessMessage->getIdentifierType(), $vendor);
+            $event = new VendorEvent(VendorState::UPDATE, [$identifier], $uploadProcessMessage->getIdentifierType(), $this->vendor->getId());
             $this->dispatcher->dispatch($event::NAME, $event);
         }
 
