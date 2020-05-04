03eba950a6c414492ddf82a8f6f92fca810b0a12
Jesper Kristensen <cableman@linuxdev.dk>
2020-05-04 22:52:53 +0200

    DDBTEAM-295: Removed wrong repository annontation on material

--- upload-api/src/Entity/Material.php
+++ upload-api/src/Entity/Material.php
@@ -34,7 +34,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
  *          }
  *     }
  * )
- * @ORM\Entity(repositoryClass="App\Repository\CoverRepository")
+ * @ORM\Entity
  */
 class Material
 {
