1c88161c195d281b3e38a27310ef3976bb52b4fd
Jesper Kristensen <cableman@linuxdev.dk>
2020-08-14 13:21:36 +0200

    DDBTEAM-572: Added missing documentation to exceptions

--- importers/src/Exception/CoverStoreAlreadyExistsException.php
+++ importers/src/Exception/CoverStoreAlreadyExistsException.php
@@ -2,10 +2,14 @@
 
 /**
  * @file
+ * Image already exists at covers store exception.
  */
 
 namespace App\Exception;
 
+/**
+ * Class CoverStoreAlreadyExistsException.
+ */
 class CoverStoreAlreadyExistsException extends CoverStoreException
 {
 }
