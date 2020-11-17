7205b96aca43a81556aec002be00859799f5119f
Jesper Kristensen <cableman@linuxdev.dk>
2020-11-17 10:14:18 +0100

    DDBTEAM-657: Remove autogenerated cover service

---
+++ importers/src/Migrations/Version20201117090905.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201117090905 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE image DROP auto_generated');
+        $this->addSql('ALTER TABLE search DROP auto_generated');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE image ADD auto_generated TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE search ADD auto_generated TINYINT(1) NOT NULL');
+    }
+}
