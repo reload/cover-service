7c95afbee1f9871039515917dddd30991f23647c
Ture Gjørup <ture@ignatz.dk>
2021-03-09 22:21:45 +0100

    DDBTEAM-728: Rename and consolidate vendor EReolenGlobal

---
+++ importers/src/Service/VendorService/OverDrive/OverDriveBooksVendorService.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * @file
+ * Service for updating book covers from OverDrive.
+ */
+
+namespace App\Service\VendorService\OverDrive;
+
+use App\Exception\IllegalVendorServiceException;
+use App\Exception\UnknownVendorServiceException;
+use App\Service\VendorService\AbstractBaseVendorService;
+use App\Service\VendorService\OverDrive\Api\Client;
+use App\Service\VendorService\ProgressBarTrait;
+use App\Utils\Message\VendorImportResultMessage;
+use App\Utils\Types\IdentifierType;
+use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\ClientInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Class OverDriveBooksVendorService.
+ */
+class OverDriveBooksVendorService extends AbstractBaseVendorService
+{
+    use ProgressBarTrait;
+
+    protected const VENDOR_ID = 14;
+
+    private $apiClient;
+    private $httpClient;
+
+    /**
+     * OverDriveBooksVendorService constructor.
+     *
+     * @param eventDispatcherInterface $eventDispatcher
+     *   Dispatcher to trigger async jobs on import
+     * @param entityManagerInterface $entityManager
+     *   Doctrine entity manager
+     * @param loggerInterface $statsLogger
+     *   Logger object to send stats to ES
+     * @param ClientInterface $httpClient
+     *   Http client to send api requests
+     * @param Client $apiClient
+     *   Api client for the OverDrive API
+     */
+    public function __construct(EventDispatcherInterface $eventDispatcher, EntityManagerInterface $entityManager, LoggerInterface $statsLogger, ClientInterface $httpClient, Client $apiClient)
+    {
+        parent::__construct($eventDispatcher, $entityManager, $statsLogger);
+
+        $this->httpClient = $httpClient;
+        $this->apiClient = $apiClient;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(): VendorImportResultMessage
+    {
+        if (!$this->acquireLock()) {
+            return VendorImportResultMessage::error(parent::ERROR_RUNNING);
+        }
+
+        $this->loadConfig();
+
+        try {
+            $this->progressStart('Starting eReolen Global import from overdrive API');
+
+            $totalCount = (0 !== $this->limit) ? $this->limit : $this->apiClient->getTotalProducts();
+
+            $batchSize = ($this->limit > 0 && $this->limit < self::BATCH_SIZE) ? $this->limit : self::BATCH_SIZE;
+            $offset = 0;
+
+            do {
+                $products = $this->apiClient->getProducts($batchSize, $offset);
+
+                $isbnImageUrlArray = [];
+                foreach ($products as $product) {
+                    $coverImageUrl = $product->images->cover->href ?? null;
+
+                    foreach ($product->formats as $format) {
+                        foreach ($format->identifiers as $identifier) {
+                            if (IdentifierType::ISBN === strtolower($identifier->type)) {
+                                if (!empty($identifier->value)) {
+                                    $isbnImageUrlArray[$identifier->value] = $coverImageUrl;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                $this->updateOrInsertMaterials($isbnImageUrlArray, IdentifierType::ISBN);
+
+                $this->progressMessageFormatted($this->totalUpdated, $this->totalInserted, $this->totalIsIdentifiers);
+                $this->progressAdvance();
+
+                $offset += self::BATCH_SIZE;
+            } while ($offset < $totalCount);
+
+            $this->logStatistics();
+
+            $this->progressFinish();
+
+            return VendorImportResultMessage::success($this->totalIsIdentifiers, $this->totalUpdated, $this->totalInserted, $this->totalDeleted);
+        } catch (\Exception $exception) {
+            return VendorImportResultMessage::error($exception->getMessage());
+        }
+    }
+
+    /**
+     * Set config from service from DB vendor object.
+     *
+     * @throws UnknownVendorServiceException
+     * @throws IllegalVendorServiceException
+     */
+    private function loadConfig(): void
+    {
+        $libraryAccountEndpoint = $this->getVendor()->getDataServerURI();
+        $clientId = $this->getVendor()->getDataServerUser();
+        $clientSecret = $this->getVendor()->getDataServerPassword();
+
+        $this->apiClient->setCredentials($libraryAccountEndpoint, $clientId, $clientSecret);
+    }
+}
