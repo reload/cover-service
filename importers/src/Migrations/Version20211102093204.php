b122eae8357cdb4b45a0b110438ac83e7ebd561b
Ture Gjørup <ture@ignatz.dk>
2021-11-03 10:37:07 +0100

    DDBTEAM-861: CS Fixes

--- importers/src/Migrations/Version20211102093204.php
+++ importers/src/Migrations/Version20211102093204.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
@@ -10,10 +12,10 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20211102093204 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE INDEX is_identifier_type_idx ON search (is_identifier, is_type)');
         $this->addSql('CREATE INDEX vendor_class_idx ON vendor (class)');
@@ -21,10 +23,10 @@ final class Version20211102093204 extends AbstractMigration
         $this->addSql('CREATE INDEX vendor_rank_idx ON vendor (rank)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP INDEX is_identifier_type_idx ON search');
         $this->addSql('DROP INDEX vendor_class_idx ON vendor');
