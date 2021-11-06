b77819c04f23a040a647227dd1d872a58d3ec711
Jesper Kristensen <cableman@linuxdev.dk>
2021-11-06 14:34:54 +0100

    DDBTEAM-861: Fixed migrations

--- importers/src/Migrations/Version20211104191514.php
+++ importers/src/Migrations/Version20211104191514.php
@@ -17,11 +17,7 @@ final class Version20211104191514 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE INDEX is_identifier_type_idx ON search (is_identifier, is_type)');
         $this->addSql('ALTER TABLE source ADD last_indexed DATETIME DEFAULT NULL');
-        $this->addSql('CREATE INDEX vendor_class_idx ON vendor (class)');
-        $this->addSql('CREATE INDEX vendor_name_idx ON vendor (name)');
-        $this->addSql('CREATE INDEX vendor_rank_idx ON vendor (rank)');
     }
 
     public function down(Schema $schema): void
@@ -29,11 +25,6 @@ final class Version20211104191514 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP INDEX is_identifier_type_idx ON search');
-        $this->addSql('CREATE INDEX is_identifier_idx ON search (is_identifier)');
         $this->addSql('ALTER TABLE source DROP last_indexed');
-        $this->addSql('DROP INDEX vendor_class_idx ON vendor');
-        $this->addSql('DROP INDEX vendor_name_idx ON vendor');
-        $this->addSql('DROP INDEX vendor_rank_idx ON vendor');
     }
 }
