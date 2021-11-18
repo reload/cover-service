90fbcfb615beee6aef1286f7c58d30e998df840a
Ture Gjørup <ture@ignatz.dk>
2021-11-18 11:02:11 +0100

    DDBTEAM-861: Fixed mismatch between database field not nullable and php propoperty

--- importers/src/Entity/Vendor.php
+++ importers/src/Entity/Vendor.php
@@ -33,7 +33,7 @@ class Vendor
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private ?string $name;
+    private string $name;
 
     /**
      * @ORM\Column(type="integer", unique=true)
@@ -121,7 +121,7 @@ class Vendor
         return $this;
     }
 
-    public function getName(): ?string
+    public function getName(): string
     {
         return $this->name;
     }
