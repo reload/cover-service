cbca1955f8aeef468020cf088a5906cdf6ee725f
Jesper Kristensen <cableman@linuxdev.dk>
2022-03-09 14:37:55 +0100

    DDBTEAM-685: Updated run docker build script

--- importers/infrastructure/docker/run.sh
+++ importers/infrastructure/docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 APP_VERSION=develop
-VERSION=latest
+VERSION=alpha
 
 docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=danskernesdigitalebibliotek/cover-service-importers:${VERSION} --file="cover-service-importers/Dockerfile" cover-service-importers
 docker build --no-cache --build-arg VERSION=${VERSION} --tag=danskernesdigitalebibliotek/cover-service-jobs:${VERSION} --file="cover-service-jobs/Dockerfile" cover-service-jobs
