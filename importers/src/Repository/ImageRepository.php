aeb1489e4730d4607845c0342bc26f590d0f0311
Jesper Kristensen <cableman@linuxdev.dk>
2022-03-16 11:16:29 +0100

    DDBTEAM-979: Removed repository comments to pass psalm

--- importers/src/Repository/ImageRepository.php
+++ importers/src/Repository/ImageRepository.php
@@ -6,12 +6,6 @@ use App\Entity\Image;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @method Image|null find($id, $lockMode = null, $lockVersion = null)
- * @method Image|null findOneBy(array $criteria, array $orderBy = null)
- * @method Image[]    findAll()
- * @method Image[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
 class ImageRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
