6e57b53f34471117899cbc0bfe724610f12cc0eb
Jesper Kristensen <cableman@linuxdev.dk>
2022-04-04 16:57:05 +0200

    DDBTEAM-990: Added PressReader vendor service

--- importers/src/Service/VendorService/PressReader/PressReaderVendorService.php
+++ importers/src/Service/VendorService/PressReader/PressReaderVendorService.php
@@ -2,13 +2,14 @@
 
 /**
  * @file
- * Use a library's data well access to get comic+ covers.
+ * Get cover from PressReader based on data well searches.
  */
 
-namespace App\Service\VendorService\DataWell;
+namespace App\Service\VendorService\PressReader;
 
-use App\Service\VendorService\DataWell\DataConverter\IversePublicUrlConverter;
+use App\Service\VendorService\DataWell\DataWellSearchService;
 use App\Service\VendorService\ProgressBarTrait;
+use App\Service\VendorService\VendorImageValidatorService;
 use App\Service\VendorService\VendorServiceInterface;
 use App\Service\VendorService\VendorServiceTrait;
 use App\Utils\Message\VendorImportResultMessage;
@@ -16,17 +17,20 @@ use App\Utils\Types\IdentifierType;
 use App\Utils\Types\VendorStatus;
 
 /**
- * Class DataWellVendorService.
+ * Class PressReaderService.
  */
-class DataWellVendorService implements VendorServiceInterface
+class PressReaderVendorService implements VendorServiceInterface
 {
     use ProgressBarTrait;
     use VendorServiceTrait;
 
-    protected const VENDOR_ID = 4;
-    private const VENDOR_ARCHIVE_NAME = 'comics+';
+    protected const VENDOR_ID = 19;
+    private const VENDOR_ARCHIVE_NAME = 'pressreader';
 
     private DataWellSearchService $datawell;
+
+    private string $urlPattern = 'https://i.prcdn.co/img?cid=%s&page=1&width=1200';
+    private VendorImageValidatorService $imageValidatorService;
 
     /**
      * DataWellVendorService constructor.
@@ -34,9 +38,10 @@ class DataWellVendorService implements VendorServiceInterface
      * @param DataWellSearchService $datawell
      *   For searching the data well
      */
-    public function __construct(DataWellSearchService $datawell)
+    public function __construct(DataWellSearchService $datawell, VendorImageValidatorService $imageValidatorService)
     {
         $this->datawell = $datawell;
+        $this->imageValidatorService = $imageValidatorService;
     }
 
     /**
@@ -59,13 +64,11 @@ class DataWellVendorService implements VendorServiceInterface
         try {
             do {
                 // Search the data well for material with acSource set to "comics plus".
-                [$pidArray, $more, $offset] = $this->datawell->search('comics plus', $offset);
+                [$pidArray, $more, $offset] = $this->datawell->search(self::VENDOR_ARCHIVE_NAME, $offset);
 
-                // Remove empty elements.
+                // Transform and clean up results.
+                $this->TransformUrls($pidArray);
                 $pidArray = array_filter($pidArray);
-
-                // Convert images url from 'medium' to 'large'
-                IversePublicUrlConverter::convertArrayValues($pidArray);
 
                 $batchSize = \count($pidArray);
                 $this->vendorCoreService->updateOrInsertMaterials($status, $pidArray, IdentifierType::PID, $this->getVendorId(), $this->withUpdatesDate, $this->withoutQueue, $batchSize);
@@ -87,7 +90,7 @@ class DataWellVendorService implements VendorServiceInterface
     }
 
     /**
-     * Set config fro service from DB vendor object.
+     * Set config from service from DB vendor object.
      */
     private function loadConfig(): void
     {
@@ -97,5 +100,36 @@ class DataWellVendorService implements VendorServiceInterface
         $this->datawell->setSearchUrl($vendor->getDataServerURI());
         $this->datawell->setUser($vendor->getDataServerUser());
         $this->datawell->setPassword($vendor->getDataServerPassword());
+    }
+
+    /**
+     * Transform the URL from the datawell to CDN https://i.prcdn.co/img?cid={$id}&page=1&width=1200.
+     *
+     * @param array $pidArray
+     *   The array of PIDs indexed by pid containing URLs.
+     */
+    private function TransformUrls(array &$pidArray): void
+    {
+        foreach ($pidArray as $pid => &$url) {
+            list($agency, $id) = explode(':', $pid);
+            $url =  sprintf($this->urlPattern, $id);
+
+            // The press reader CDN insert at special image saying that the content is not updated for newest news
+            // cover. See https://i.prcdn.co/img?cid=9L09&page=1&width=1200, but the size will be under 30Kb, so we have
+            // this extra test.
+            $header = $this->imageValidatorService->remoteImageHeader('cf-polished', $url);
+            if (!empty($header)) {
+                $header = reset($header);
+                list($label, $size) = explode('=', $header);
+                if ($size < 30000) {
+                    // Size to little set it to null.
+                    $url = null;
+                }
+            }
+            else {
+                // Size header not found.
+                $url = null;
+            }
+        }
     }
 }
