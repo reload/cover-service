0a11299cac95f3d7de5d19a0958da0af43c064e7
Jesper Kristensen <cableman@linuxdev.dk>
2022-11-21 14:22:02 +0100

    COVER-7: Added logic to "remove-by-url-pattern" command

--- importers/src/Command/Vendors/VendorRemoveByUrlPatternCommand.php
+++ importers/src/Command/Vendors/VendorRemoveByUrlPatternCommand.php
@@ -2,13 +2,18 @@
 
 namespace App\Command\Vendors;
 
+use App\Message\DeleteMessage;
+use App\Repository\SourceRepository;
+use App\Utils\Types\VendorState;
+use Doctrine\ORM\Query;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsCommand(
     name: 'app:vendor:remove-by-url-pattern',
@@ -16,34 +21,72 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class VendorRemoveByUrlPatternCommand extends Command
 {
+    private const LIMIT = 100;
 
-    // @todo Find existing and remove them
-    // SELECT * FROM `cover-service`.`source` WHERE (`vendor_id` = 14) AND (`original_file` LIKE '%{00000000-0000-0000-0000-000%') ORDER BY `original_file` DESC LIMIT 300 OFFSET 0;
-    // SELECT DISTINCT(original_file) FROM `cover-service`.`source` WHERE (`vendor_id` = 14) AND (`original_file` LIKE '%{00000000-0000-0000-0000-000%') ORDER BY `original_file` DESC LIMIT 300 OFFSET 0;
+    public function __construct(
+        private readonly MessageBusInterface $bus,
+        private readonly SourceRepository $sourceRepository,
+    ) {
+        parent::__construct();
+    }
 
     protected function configure(): void
     {
-        $this
-            ->addArgument('vendorId', InputArgument::REQUIRED, 'Vendor id found in the database');
-            
-        ;
+        $this->setDefinition(
+            new InputDefinition([
+                new InputOption('vendor-id', null, InputOption::VALUE_REQUIRED, 'Vendor id found in the database', 0),
+                new InputOption('pattern', null, InputOption::VALUE_REQUIRED, 'Original image file pattern', '%{00000000-0000-0000-0000-000%'),
+            ])
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $vendorId = (int) $input->getOption('vendor-id');
+        $pattern = (string) $input->getOption('pattern');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $progressBar = new ProgressBar($output);
+        $progressBar->setFormat('debug');
+        $progressBar->start();
+
+        $query = $this->getSources($vendorId, $pattern);
+
+        foreach ($query->toIterable() as $source) {
+            $message = new DeleteMessage();
+            $message->setOperation(VendorState::DELETE)
+                ->setIdentifier($source->getMatchId())
+                ->setIdentifierType($source->getMatchType())
+                ->setVendorId($source->getVendor()->getId());
+
+            $this->bus->dispatch($message);
+            $progressBar->advance();
         }
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
+        $progressBar->finish();
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        // Send CLI cursor to empty line.
+        $output->writeln('');
 
         return Command::SUCCESS;
+    }
+
+    /**
+     * Get Sources with pattern in original filename limit to vendor.
+     *
+     * @param int $vendorId
+     * @param string $pattern
+     *
+     * @return Query
+     */
+    public function getSources(int $vendorId, string $pattern): Query
+    {
+        $queryBuilder = $this->sourceRepository->createQueryBuilder('s')
+            ->where('s.vendor = :vendorId')
+            ->andWhere('s.originalFile LIKE :pattern')
+            ->orderBy('s.originalFile', 'DESC')
+            ->setParameter('vendorId', $vendorId)
+            ->setParameter('pattern', $pattern);
+
+        return $queryBuilder->getQuery();
     }
 }
