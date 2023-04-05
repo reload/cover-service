762c6b9e6d5957b74659a9db84ad83385032acff
Ture Gjørup <ture@ignatz.dk>
2023-04-05 13:56:06 +0200

    COVER-41: Fixed escaping in migration

--- importers/migrations/Version20230404080408.php
+++ importers/migrations/Version20230404080408.php
@@ -20,12 +20,12 @@ final class Version20230404080408 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("UPDATE vendor SET class = 'App\\Service\\VendorService\\OpenLibrary\\OpenLibraryVendorService', name = 'OpenLibrary' WHERE id = 20");
+        $this->addSql("UPDATE vendor SET class = 'App\\\Service\\\VendorService\\\OpenLibrary\\\OpenLibraryVendorService', name = 'OpenLibrary' WHERE id = 20");
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql("UPDATE vendor SET class = 'App\\Service\\VendorService\\OpenLibrary\\OpenLibraryVendor', name = 'OpenLibraryVendor' WHERE id = 20");
+        $this->addSql("UPDATE vendor SET class = 'App\\\Service\\\VendorService\\\OpenLibrary\\\OpenLibraryVendor', name = 'OpenLibraryVendor' WHERE id = 20");
     }
 }
